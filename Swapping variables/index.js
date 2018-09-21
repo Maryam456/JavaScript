692ed68ccc0675206f6a94668da572b8bf3157e2
Maryam456 <37376125+Maryam456@users.noreply.github.com>
2018-09-21 14:03:56 -0600

    update

--- Swapping variables/index.js
+++ Swapping variables/index.js
@@ -1,22 +1,12 @@
-console.log("Hello World");
-//create 10 variables about yourself
-var name= "Maryam";
-var last name="Ali Mohamud";
-let hair colour= "Dark Brown";
-let horoscope= "Libra";
-let age= "15";
-const ageAfraid = 15; //security feature
-let birthdate= "October 6 2002";
-let favourite book= "none";//undefined, null
-let femlae= true;
-//print variables to console
-console.log(name);
-console.log(last name);
-console.log(hair colour);
-console.log(horoscope);
-console.log(age);
-console.log(ageAfraid);
-console.log(birthdate);
-console.log(favourite book)
-console.log(female);
-console.log("am I male? ")
+let firstName = "Maryam"
+let LasName = "Ali Mohamud"
+
+//new code goes here
+// Add a third variable
+let memory = firstName;
+firstName = lastName;
+lastName = memory;
+
+// Print variables toconsole
+console.log("My first name is: " + firstname);
+console.log("My last name is: " + lastName )
--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 console.log("Hello World");
+
 //create 10 variables about yourself
-let name= "Maryam";
-let last name="Ali Mohamud";
-let hair colour= "Dark Brown";
+let MaryamAliMohamud = {
+ lastName="Ali Mohamud",
+ haircolour= "Dark Brown",
+ horoscope= "Libra",
+ ageAfraid = 15,
+  birthdate= "October 6 2002",
+ favouritebook= null,
+
+}
+let lastname="Ali Mohamud";
+let haircolour= "Dark Brown";
 let horoscope= "Libra";
 let age= "15";
 const ageAfraid = 15; //security feature
 let birthdate= "October 6 2002";
-let favourite book= null;//undefined, null; originally "none"
-let femlae= true;
+let favourite book= null; //undefined, null; originally "none"
+let Am I female ;
 //print variables to console
 console.log(name);
 console.log(last name);
