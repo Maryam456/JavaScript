9c4ed745e8b6aeb239c9e96fce4172048a3bd06f
Maryam456 <37376125+Maryam456@users.noreply.github.com>
2018-09-25 13:29:02 -0600

    update

---
+++ Swapping Varables Object Start/index.js
@@ -0,0 +1,34 @@
+console.log("Hello World");
+
+//create 10 variables about yourself
+let MaryamAliMohamud =[];
+
+// firstName, Lastname, age, ageAfriad, hair, female
+MaryamAliMohamud = ["AliMohamud", 15]
+
+{
+ lastName: "Ali Mohamud",
+ haircolour: "darkbrown",
+ horoscope: "Libra",
+ ageAfraid:  15,
+  birthdate: "October 6 2002",
+ favouritebook: null,
+
+}
+let lastname="Ali Mohamud";
+let haircolour= "Dark Brown";
+let horoscope= "Libra";
+let age= "15";
+let birthdate= "October 6 2002";
+let favouritebook= null; //undefined, null; originally "none"
+let Am I female;
+//print variables to console
+console.log(name);
+console.log(last name);
+console.log(hair colour);
+console.log(horoscope);
+console.log(age);
+console.log(ageAfraid);
+console.log(birthdate);
+console.log(favourite book);
+console.log("Am I female" + true);
